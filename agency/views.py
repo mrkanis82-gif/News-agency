499713728e0f640fb0cc275518570fb4331a0964
Tymur <mr.kanis82@gmail.com>
2026-08-11 17:15:49 -0400

    created index in views.py that will work as home page. It will show num_topics, num_redactors, num_news_paper. Also add url path for it and empty template file.

--- agency/views.py
+++ agency/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
 
-# Create your views here.
+from agency.models import Topic, Redactor, NewsPaper
+
+
+
+def index(request):
+    """View function for the home page of the site."""
+
+    num_topics = Topic.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_news_paper = NewsPaper.objects.count()
+
+
+    context = {
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+        "num_news_paper": num_news_paper,
+    }
+
+    return render(request, "agency/index.html", context=context)

0560c023abbb7fefb490b619dcf8405735ac6732
Tymur <mr.kanis82@gmail.com>
2026-08-11 14:50:40 -0400

    add model Redactor(AbstractUser) and implement it as AUTH_USER_MODEL of the Project also add ordering to Topic model.

--- agency/models.py
+++ agency/models.py
@@ -1,7 +1,24 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Topic(models.Model):
     name = models.CharField(max_length=250, unique=True)
 
+    class Meta:
+        ordering = ["name"]
+
     def __str__(self):
         return self.name
+
+
+class Redactor(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+
+    class Meta:
+        verbose_name = "redactor"
+        verbose_name_plural = "redactors"
+
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"

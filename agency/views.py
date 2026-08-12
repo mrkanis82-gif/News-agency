aac414b84b7162411ae250c0d268b478b3f29d4d
Tymur <mr.kanis82@gmail.com>
2026-08-12 14:42:12 -0400

    add basic CRUD views for Topic and Redactor updated urls for them and create empty templates for them.

--- agency/views.py
+++ agency/views.py
@@ -32,6 +32,7 @@ class NewsPaperListView(LoginRequiredMixin, generic.ListView):
 
 class NewsPaperCreateView(LoginRequiredMixin, generic.CreateView):
     model = NewsPaper
+    template_name = "agency/form_newspaper.html"
     success_url = reverse_lazy("agency:newspapers_list")
 
 
@@ -53,7 +54,43 @@ class RedactorListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 10
 
 
+class RedactorCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Redactor
+    success_url = reverse_lazy("agency:redactors_list")
+    template_name = "agency/form_redactor.html"
+
+
+class RedactorUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Redactor
+    success_url = reverse_lazy("agency:redactors_list")
+    template_name = "agency/form_redactor.html"
+
+
+class RedactorDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Redactor
+    template_name = "agency/confirm_delete.html"
+    success_url = reverse_lazy("agency:redactors_list")
+
+
 class TopicListView(LoginRequiredMixin, generic.ListView):
     model = Topic
     template_name = "agency/topics_list.html"
     paginate_by = 10
+
+
+class TopicCreateView(LoginRequiredMixin, generic.CreateView):
+    model = Topic
+    success_url = reverse_lazy("agency:topics_list")
+    template_name = "agency/form_topic.html"
+
+
+class TopicUpdateView(LoginRequiredMixin, generic.UpdateView):
+    model = Topic
+    template_name = "agency/form_topic.html"
+    success_url = reverse_lazy("agency:topics_list")
+
+
+class TopicDeleteView(LoginRequiredMixin, generic.DeleteView):
+    model = Topic
+    template_name = "agency/confirm_delete.html"
+    success_url = reverse_lazy("agency:topics_list")

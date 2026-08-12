22a008bec5f0b3494f01b7de23601034e212a588
Tymur <mr.kanis82@gmail.com>
2026-08-12 14:43:59 -0400

    fixed url mistake, wrong file

--- agency/urls.py
+++ agency/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path
 
+from . import views
 from .views import index
 
 urlpatterns = [
     path("", index, name='index'),
+    path("topics/", views.TopicListView.as_view(), name="topic-list"),
+    path("topics/create/", views.TopicCreateView.as_view(), name="topic-create"),
+    path("topics/<int:pk>/delete/", views.TopicDeleteView.as_view(), name="topic-delete"),
+    path("topics/<int:pk>/update/", views.TopicUpdateView.as_view(), name="topic-update"),
+    path("newspaper/", views.NewsPaperListView.as_view(), name="newspaper-list"),
+    path("newspaper/create/", views.NewsPaperCreateView.as_view(), name="newspaper-create"),
+    path("newspaper/<int:pk>/delete/", views.NewsPaperDeleteView.as_view(), name="newspaper-delete"),
+    path("newspaper/<int:pk>/update/", views.NewsPaperUpdateView.as_view(), name="newspaper-update"),
+    path("redactor/", views.RedactorListView.as_view(), name="redactor-list"),
 ]
 
 app_name = "agency"

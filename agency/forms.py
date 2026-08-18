fde7a5836b1a767a89ae248127902c1112ba00c3
Tymur <mr.kanis82@gmail.com>
2026-08-18 11:00:17 -0400

    add newspaper creation form to add ModelMultipleChoiceField for publishers field

--- agency/forms.py
+++ agency/forms.py
@@ -1,9 +1,21 @@
+from django import forms
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from agency.models import Redactor
+from agency.models import NewsPaper, Redactor
 
 
 class RedactorCreationForm(UserCreationForm):
     class Meta:
         model = Redactor
         fields = ("username", "first_name", "last_name", "years_of_experience")
+
+
+class NewspaperCreationForm(forms.ModelForm):
+    publishers = forms.ModelMultipleChoiceField(
+        queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    class Meta:
+        fields = "__all__"
+        model = NewsPaper
--- agency/views.py
+++ agency/views.py
@@ -36,8 +36,7 @@ class NewsPaperDetailView(LoginRequiredMixin, generic.DetailView):
     template_name = "agency/newspaper_detail.html"
 
 class NewsPaperCreateView(LoginRequiredMixin, generic.CreateView):
-    model = NewsPaper
-    fields = "__all__"
+    form_class = forms.NewspaperCreationForm
     exclude = ["date"]
     template_name = "agency/form_newspaper.html"
     success_url = reverse_lazy("agency:newspaper-list")

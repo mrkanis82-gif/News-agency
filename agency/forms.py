67780bcdac96b76fbc6c76e775a693a39d38ef55
Tymur <mr.kanis82@gmail.com>
2026-08-21 12:10:07 -0400

    make topics as MultipleChoiceField in NewspaperForm to accomplish additional task

--- agency/forms.py
+++ agency/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
-from agency.models import NewsPaper, Redactor
+from agency.models import NewsPaper, Redactor, Topic
 
 
 class RedactorCreationForm(UserCreationForm):
@@ -14,6 +14,10 @@ class RedactorCreationForm(UserCreationForm):
 class NewspaperForm(forms.ModelForm):
     publishers = forms.ModelMultipleChoiceField(
         queryset=get_user_model().objects.all(),
+        widget=forms.CheckboxSelectMultiple,
+    )
+    topics = forms.ModelMultipleChoiceField(
+        queryset=Topic.objects.all(),
         widget=forms.CheckboxSelectMultiple,
     )
     class Meta:

8dcf9c7faa356e1c75e4bd5ef6233b0215d5a06c
Tymur <mr.kanis82@gmail.com>
2026-08-21 18:39:35 -0400

    add crispy bootstrap5: CRISPY_ALLOWED_TEMPLATE_PACKS, CRISPY_TEMPLATE_PACK, INSTALLED_APPS.

--- News_agency/settings.py
+++ News_agency/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "crispy_forms",
+    "crispy_bootstrap5",
     "bootstrap5",
     "agency",
 ]
@@ -69,6 +71,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "News_agency.wsgi.application"
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+
+CRISPY_TEMPLATE_PACK = "bootstrap5"
 
 
 # Database

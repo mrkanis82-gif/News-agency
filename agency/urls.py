67bc364cc58f8d8603e5673a33b2b6236c092b36
Tymur <mr.kanis82@gmail.com>
2026-08-22 15:10:44 -0400

    change imports to absolute imports

--- agency/urls.py
+++ agency/urls.py
@@ -1,8 +1,8 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path
 
-from . import views
-from .views import index
+from agency import views
+from agency.views import index
 
 urlpatterns = [
     path("login/", LoginView.as_view(), name="login"),
